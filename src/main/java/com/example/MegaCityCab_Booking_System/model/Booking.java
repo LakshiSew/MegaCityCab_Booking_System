54ce40f78284537cb22435980df3586de9939ed4
LakshiSew <lakshi8999@gmail.com>
2025-06-18 22:58:32 +0530

    booking entity done

---
+++ src/main/java/com/example/MegaCityCab_Booking_System/model/Booking.java
@@ -0,0 +1,25 @@
+package com.example.MegaCityCab_Booking_System.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Document(collection = "bookings")
+public class Booking {
+  
+    private String id;
+    private String bookingNumber;
+    private String customerId; // Link to the Customer who made the booking
+    private String driverId; // Link to the Driver assigned to the booking
+    private String cabId; // Link to the Cab used for the booking
+    private String pickupLocation;
+    private String dropoffLocation;
+     private LocalDateTime bookingTime;// Timestamp of when the booking was made
+    private String status; // e.g., "Pending", "Completed", "Cancelled"
+
+}
